6fbab72f19254fcedee30d31ebac0831301adeca
ashiqurras <ashiqurrussel2@gmail.com>
2023-02-05 15:53:41 +0100

    handling ui data using filter from redux store

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -5,7 +5,7 @@ import { toggleBrand, toggleStock } from "../../redux/actions/filterActions";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
-  console.log(products);
+
   useEffect(() => {
     fetch("/products.json")
       .then((res) => res.json())
@@ -13,12 +13,38 @@ const Home = () => {
         setProducts(data);
       });
   }, []);
-
+  console.log(products);
+  let contents;
   const filters = useSelector((state) => state.filter.filters);
   const { brands, stock } = filters;
   const dispatch = useDispatch();
+
   console.log(filters);
   const activeClass = "text-white  bg-indigo-500 border-white";
+
+  if (products.length) {
+    contents = products?.map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ));
+  }
+
+  if (products.length && (stock || brands.length)) {
+    contents = products
+      .filter((product) => {
+        if (stock) {
+          return product.status === true;
+        }
+        return product;
+      })
+      .filter((product) => {
+        if (brands.length) {
+          return brands.includes(product.brand);
+        } else {
+          return product;
+        }
+      })
+      .map((product) => <ProductCard key={product.model} product={product} />);
+  }
 
   return (
     <div className="max-w-7xl gap-14 mx-auto my-10">
@@ -49,9 +75,7 @@ const Home = () => {
         </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
-        {products?.map((product) => (
-          <ProductCard key={product.model} product={product} />
-        ))}
+        {contents}
       </div>
     </div>
   );
