7beee8d6552d312b0e42163b1ec064b9074c8316
ashiqurras <ashiqurrussel2@gmail.com>
2023-02-05 01:01:04 +0100

    dynamic toggle

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
-import { toggleBrand } from "../../redux/actions/filterActions";
+import { toggleBrand, toggleStock } from "../../redux/actions/filterActions";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -14,26 +14,35 @@ const Home = () => {
       });
   }, []);
 
+  const filters = useSelector((state) => state.filter.filters);
+  const { brands, stock } = filters;
   const dispatch = useDispatch();
-
+  console.log(filters);
   const activeClass = "text-white  bg-indigo-500 border-white";
 
   return (
     <div className="max-w-7xl gap-14 mx-auto my-10">
       <div className="mb-10 flex justify-end gap-5">
         <button
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            stock ? activeClass : "null"
+          }`}
+          onClick={() => dispatch(toggleStock())}
         >
           In Stock
         </button>
         <button
-          className={`border px-3 py-2 rounded-full font-semibold`}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("amd") ? activeClass : "null"
+          }`}
           onClick={() => dispatch(toggleBrand("amd"))}
         >
           AMD
         </button>
         <button
-          className={`border px-3 py-2 rounded-full font-semibold`}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("intel") ? activeClass : "null"
+          }`}
           onClick={() => dispatch(toggleBrand("intel"))}
         >
           Intel
